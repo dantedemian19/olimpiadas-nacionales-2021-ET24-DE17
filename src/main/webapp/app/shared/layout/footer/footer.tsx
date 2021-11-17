ccee4174d79a9590bdc8c85be08044b4d5cc7e41
dantedemian19 <dantedemian19@gmail.com>
2021-11-17 10:33:23 -0300

    cambios muy pero muy importantes

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -8,7 +8,7 @@ const Footer = props => (
   <div className="footer page-content">
     <Row>
       <Col md="12">
-        <p>Your footer</p>
+        <p>Your footer mother</p>
       </Col>
     </Row>
   </div>
