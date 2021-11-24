11dc3646ba7a439ef8fe7ec304edfa65ff9205bf
MattGimenez <matgimenez53@gmail.com>
2021-11-24 16:19:34 -0300

    Fix NestJs changes on updated Leches entity

--- server/src/service/dto/leches.dto.ts
+++ server/src/service/dto/leches.dto.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ApiModelProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MinLength, MaxLength, Length, Min, Max, Matches } from 'class-validator';
 import { BaseDTO } from './base.dto';
 
@@ -10,30 +10,30 @@ import { CisternasDTO } from './cisternas.dto';
  */
 export class LechesDTO extends BaseDTO {
     @IsNotEmpty()
-    @ApiModelProperty({ description: 'analisis field' })
+    @ApiProperty({ description: 'analisis field' })
     analisis: string;
 
     @IsNotEmpty()
-    @ApiModelProperty({ description: 'calidad field' })
+    @ApiProperty({ description: 'calidad field' })
     calidad: number;
 
     @IsNotEmpty()
-    @ApiModelProperty({ description: 'cantidad field' })
+    @ApiProperty({ description: 'cantidad field' })
     cantidad: number;
 
     @IsNotEmpty()
-    @ApiModelProperty({ description: 'fechaDeIngreso field' })
+    @ApiProperty({ description: 'fechaDeIngreso field' })
     fechaDeIngreso: any;
 
     @IsNotEmpty()
-    @ApiModelProperty({ description: 'tambo field' })
+    @ApiProperty({ description: 'tambo field' })
     tambo: string;
 
     @IsNotEmpty()
-    @ApiModelProperty({ description: 'temperatura field' })
+    @ApiProperty({ description: 'temperatura field' })
     temperatura: number;
 
-    @ApiModelProperty({ type: CisternasDTO, description: 'cisterna relationship' })
+    @ApiProperty({ type: CisternasDTO, description: 'cisterna relationship' })
     cisterna: CisternasDTO;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
